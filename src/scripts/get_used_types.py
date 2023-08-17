67478337c07ca48e0cbd4de746eee6c82b793564
Jonas <jonas.lederer@tu-berlin.de>
2023-08-17 15:04:39 +0200

    qm9 eval is working

--- src/scripts/get_used_types.py
+++ src/scripts/get_used_types.py
@@ -4,6 +4,7 @@ import torch
 from tqdm import tqdm
 import collections
 import matplotlib.pyplot as plt
+from shutil import rmtree
 
 from moinn.clustering.utils.coarse_graining import spatial_assignments, get_hard_assignments
 from moinn.clustering.neighbors import PairwiseDistances, AdjMatrix
@@ -18,6 +19,15 @@ def get_used_types(loader, model, device=torch.device('cuda')):
 
         type_ass = results["type_assignments"].detach()
 
+        if batch_idx == 0:
+            max_n_clusters = type_ass.to(torch.device("cpu")).shape[-1]
+            tot_ass = torch.zeros((30, max_n_clusters))
+
+        # sum up type assignments
+        n_nodes = type_ass.shape[1]
+        for ass in type_ass.cpu():
+            tot_ass[:n_nodes, :] += ass
+
         max_in_sample = torch.max(type_ass, dim=1)[0]
         max_in_batch = torch.max(max_in_sample, dim=0, keepdim=True)[0]
 
@@ -30,7 +40,9 @@ def get_used_types(loader, model, device=torch.device('cuda')):
 
         mask = max_in_batch.cpu() > 0.05
 
-    return mask[0]
+        filled_clusters = tot_ass.sum(dim=0)
+
+    return mask[0], filled_clusters
 
 
 def get_moieties(loader, model, mask, device=torch.device('cuda')):
@@ -112,7 +124,7 @@ def get_moieties(loader, model, mask, device=torch.device('cuda')):
                 bead_name_tmp += k + str(v)
         all_substructures_dense.append(bead_name_tmp)
     # get topk substructures for each cluster
-    topk = 3
+    topk = 5
     count_values, substruc_indices = counts.topk(k=topk, dim=0)
 
     substruc_indices = substruc_indices.transpose(0, 1)[mask]
@@ -122,23 +134,41 @@ def get_moieties(loader, model, mask, device=torch.device('cuda')):
     count_values = count_values.transpose(0, 1)
     count_values = count_values / count_values.sum(0) * 100.
 
-    cmap = plt.get_cmap("tab20")
-    colors_dict = torch.unique(used_substructures).tolist()
-    colors_dict = {struc_idx: cmap(color_idx) for color_idx, struc_idx in enumerate(colors_dict)}
+    return substruc_indices, count_values, all_substructures_dense
 
-    colors = [[] for _ in range(len(used_substructures))]
-    for row_idx, row in enumerate(used_substructures):
-        row_tmp = []
-        for col_idx, struc_idx in enumerate(row):
-            if count_values[row_idx][col_idx].item() > 1.:
-                row_tmp.append(all_substructures_dense[struc_idx]+", %d %%" % (count_values[row_idx][col_idx].item()))
-                colors[row_idx].append(colors_dict[struc_idx])
-            else:
-                row_tmp.append("")
-                colors[row_idx].append("w")
-        used_substructures[row_idx] = row_tmp
 
-    return substruc_indices, all_substructures_dense, count_values
+def plot_table(figname, filled_clusters, cellText, rows, colors_text=None):
+
+    fig = plt.figure(figsize=(25, 14))
+
+    # bar plot of frequency of cluster types
+    if colors_text is not None:
+        cmap = plt.get_cmap("tab20")
+        colors = [cmap(idx) for idx in range(filled_clusters.shape[0])]
+        plt.bar(range(filled_clusters.shape[0]), filled_clusters.numpy(), 0.4, color=colors)
+    else:
+        plt.bar(range(filled_clusters.shape[0]), filled_clusters.numpy(), 0.4)
+    # Add a table at the bottom of the axes
+    if colors_text is not None:
+        the_table = plt.table(cellText=cellText,
+                              rowLabels=rows,
+                              cellColours=colors_text,
+                              loc='bottom',
+                              cellLoc="right")
+    else:
+        the_table = plt.table(cellText=cellText,
+                              rowLabels=rows,
+                              loc='bottom',
+                              cellLoc="center")
+    the_table.scale(0.93, 2.6)
+
+    # Adjust layout to make room for the table:
+    plt.subplots_adjust(left=0.2, bottom=0.2)
+
+    plt.title('Cluster Type Usage')
+    plt.ylabel('#atoms assigned to certain type')
+    plt.xticks([])
+    fig.savefig(figname)
 
 
 if __name__ == "__main__":
@@ -149,6 +179,15 @@ if __name__ == "__main__":
     batch_size = 1
 
     atom_names_dict = {1: "H", 6: "C", 7: "N", 8: "O", 9: "F", 16: "S"}
+
+    eval_dir = os.path.join(mdir, "eval_images")
+
+    # create directory
+    tmp_dir = os.path.join(eval_dir, "tmp")
+    if os.path.exists(tmp_dir):
+        print("existing evaluation results will be overwritten...")
+        rmtree(tmp_dir)
+    os.makedirs(tmp_dir)
 
     # load dataset splits
     dataset = spk.AtomsData(dpath)
@@ -165,10 +204,57 @@ if __name__ == "__main__":
     modelpath = os.path.join(mdir, "best_model")
     model = torch.load(modelpath, map_location=device)
 
-    used_types = get_used_types(test_loader, model, device)
+    used_types, filled_clusters = get_used_types(test_loader, model, device)
 
-    used_substructures, all_substructures_dense, count_values = get_moieties(test_loader, model, used_types, device)
+    substruc_indices, count_values, all_substructures_dense = get_moieties(test_loader, model, used_types, device)
 
-    
+    n_used_types = substruc_indices.shape[1]
+
+    cmap = plt.get_cmap("tab20")
+    colors_dict = torch.unique(substruc_indices).tolist()
+    colors_dict = {struc_idx: cmap(color_idx) for color_idx, struc_idx in enumerate(colors_dict)}
+
+    substruc_indices = substruc_indices.tolist()
+
+    colors = [[] for _ in range(len(substruc_indices))]
+    for row_idx, row in enumerate(substruc_indices):
+        row_tmp = []
+        for col_idx, struc_idx in enumerate(row):
+            if count_values[row_idx][col_idx].item() > 1.:
+                row_tmp.append(all_substructures_dense[struc_idx]+", %d %%" % (count_values[row_idx][col_idx].item()))
+                colors[row_idx].append(colors_dict[struc_idx])
+            else:
+                row_tmp.append("")
+                colors[row_idx].append("w")
+        substruc_indices[row_idx] = row_tmp
+
+    # rows
+    rows = ["cluster-type"]
+    for _ in range(5):
+        rows.append("sub-struc. {}".format(_))
+    # table entries
+    cellText = [[str(_) for _ in range(n_used_types)]]
+    cellText += substruc_indices
+    # table cell coloring
+    colors_text = [['w' for _ in range(n_used_types)]]
+    colors_text += colors
+
+    # in case we do not want to color all substructures
+    n_colored_rows = 0
+    for idx in range(n_colored_rows + 1, len(colors_text)):
+        colors_text[idx] = ['w' for _ in range(n_used_types)]
+
+    ########################################################################################################################
+    # plot
+    ########################################################################################################################
+
+    # show most common substructures
+    plot_table(
+        figname=os.path.join(eval_dir, "common_substructures.pdf"),
+        filled_clusters=filled_clusters[used_types],
+        cellText=cellText,
+        rows=rows,
+        colors_text=colors_text
+    )
 
     print()
